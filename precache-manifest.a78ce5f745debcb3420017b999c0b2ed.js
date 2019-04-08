82503c8bf3af6b2c3a5640a4cb8dcfbeaa1d88b6
Arvind Thangamani <dnivra26@gmail.com>
2019-04-08 14:12:11 +0530

    github pages

---
+++ precache-manifest.a78ce5f745debcb3420017b999c0b2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89ffae1c492fc2d42286ad0a1fbac23f",
+    "url": "/y_so_serious/static/media/joker.89ffae1c.jpg"
+  },
+  {
+    "revision": "f391b5e7cbf5ba3c3ca4",
+    "url": "/y_so_serious/static/js/runtime~main.6bf8ba94.js"
+  },
+  {
+    "revision": "da4e95fcfeca3879111b",
+    "url": "/y_so_serious/static/js/main.7eb150b9.chunk.js"
+  },
+  {
+    "revision": "f829bd9124e29f8e8365",
+    "url": "/y_so_serious/static/js/2.cac434e0.chunk.js"
+  },
+  {
+    "revision": "da4e95fcfeca3879111b",
+    "url": "/y_so_serious/static/css/main.c3ab8830.chunk.css"
+  },
+  {
+    "revision": "17f15c1be01527b789230fc0e493289d",
+    "url": "/y_so_serious/index.html"
+  }
+];
