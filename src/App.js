43ad486c1e138cebde7ed41f998bdd03dd2ab8db
Arvind Thangamani <dnivra26@gmail.com>
2019-04-08 16:49:14 +0530

    add alts for img tags

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ class App extends Component {
     image.src = this.webcam.getScreenshot();
     const detectionsWithExpressions = await faceapi.detectAllFaces(image).withFaceExpressions();
     console.log(detectionsWithExpressions)
-    if(detectionsWithExpressions && detectionsWithExpressions.length != 0) {
+    if(detectionsWithExpressions && detectionsWithExpressions.length !== 0) {
       this.setState(
         {
           image: image.src,
           expresion: orderBy(detectionsWithExpressions[0].expressions, ['probability'], ['desc'])[0]['expression']
         }
         );
-      if(this.state.expresion == "neutral") {
+      if(this.state.expresion === "neutral") {
           this.setState({oldImage: image.src});
       }
     }
@@ -47,9 +47,9 @@ class App extends Component {
     clearInterval(this.state.timerId);
     return (
       <div className="container">
-        <img className="joker" src="https://media.giphy.com/media/KEVNWkmWm6dm8/giphy.gif" />
-        <img className="happy" src={this.state.image} />
-        <img className="nothappy" src={this.state.oldImage} />
+        <img alt="you won" className="joker" src="https://media.giphy.com/media/KEVNWkmWm6dm8/giphy.gif" />
+        <img alt="happy" className="happy" src={this.state.image} />
+        <img alt="not happy" className="nothappy" src={this.state.oldImage} />
       </div>
     )
   }
@@ -62,7 +62,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.expresion === "happy" ? this.getSuccess() : <img className="joker" src={joker}/>}
+          {this.state.expresion === "happy" ? this.getSuccess() : <img alt="joker" className="joker" src={joker}/>}
           <Webcam
             audio={false}
             height={350}
