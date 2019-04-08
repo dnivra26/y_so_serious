1d209bf48442bb237a13652af41b3e96d6f1823e
Arvind Thangamani <dnivra26@gmail.com>
2019-04-08 12:53:27 +0530

    detect expression in webcam

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as faceapi from 'face-api.js';
+import Webcam from "react-webcam";
+import orderBy from 'lodash.orderby'
 import './App.css';
 
 class App extends Component {
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+  constructor(){
+    super();
+    this.state = {
+      expresion: ""
+    }
+    this.webcam = React.createRef();
+  }
+  async componentDidMount(){
+    await faceapi.loadFaceDetectionModel("/models")
+    await faceapi.loadFaceExpressionModel("/models")
+    await faceapi.loadSsdMobilenetv1Model('/models')
+  }
+  analyse = async () => {
+    this.setState({expresion: ""});
+    var image = new Image();
+    image.src = this.webcam.getScreenshot();
+    const detectionsWithExpressions = await faceapi.detectAllFaces(image).withFaceExpressions();
+    this.setState({expresion: orderBy(detectionsWithExpressions[0].expressions, ['probability'], ['desc'])[0]['expression']});
+    console.log(detectionsWithExpressions)
+  }
   render() {
+    const videoConstraints = {
+      width: 350,
+      height: 350,
+      facingMode: 'user',
+    };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Webcam
+            audio={false}
+            height={350}
+            ref={this.setRef}
+            screenshotFormat="image/jpeg"
+            width={350}
+            videoConstraints={videoConstraints}
+          />
+          <div className="expression">{this.state.expresion ? this.state.expresion : ""}</div>
+          <button onClick={() => {this.analyse()}} >Detect</button>
         </header>
       </div>
     );
